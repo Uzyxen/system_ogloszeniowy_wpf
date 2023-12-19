67bde3d8c9bdb2b335e73f3825e8c4fb3e094b50
Uzyxen <michal912456@gmail.com>
2023-12-19 21:46:54 +0100

    added myApplications page

---
+++ system_ogloszeniowy_wpf/Pages/MyApplications.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace system_ogloszeniowy_wpf.Pages
+{
+    /// <summary>
+    /// Logika interakcji dla klasy MyApplications.xaml
+    /// </summary>
+    public partial class MyApplications : Page
+    {
+        public MyApplications()
+        {
+            InitializeComponent();
+        }
+    }
+}
